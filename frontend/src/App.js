de5a93f49cb9ecd81feb41e790fa947016ea8d98
Tulaib <tulluahsid@gmail.com>
2022-03-16 14:32:14 +0500

    newPush

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div >
+      <header >
+          <a href='/'>Amazona</a>
+      </header>
+      <main>List Productsssswss</main>
+    </div>
+  );
+}
+
+export default App;
